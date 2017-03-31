c3169ea13953c13602d36c676ff12b67264b4ac1
kaneshirok <yakyu3214@gmail.com>
2017-03-31 23:57:25 +0900

    logic modified

--- Calculator/app/src/main/java/com/example/kaneshirok/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/kaneshirok/calculator/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.kaneshirok.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
+
+    /** 入力テキスト1 */
+    private EditText inputNumber1;
+
+    /** 入力テキスト2 */
+    private EditText inputNumber2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
     }
 }
